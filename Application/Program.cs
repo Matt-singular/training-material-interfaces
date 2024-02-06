cec502e16e70b8c83d084875ae733253cef4a64b
Matthew Lewis <mlewis@singular.co.za>
2024-02-06 10:31:14 +0200

    Updated the startup to call one of the services - DI example

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,11 +1,16 @@
 ﻿using Application;
 using Microsoft.Extensions.DependencyInjection;
+using Services.Example2;
 
 // Dependency Injection
 var serviceProvider = new ServiceCollection()
   .AddExampleServices()
   .BuildServiceProvider();
 
-// Execute Examples - Not used
-//var applicationOrchestration = serviceProvider.GetRequiredService<IOrchestration>();
-//applicationOrchestration.Process();
+// Execute Examples - Demonstrate dependency injection
+var addOperation = serviceProvider.GetRequiredService<IAddOperation>();
+var result = addOperation.Execute(5, 6);
+
+// Write the results and keep the terminal open
+Console.Write(result);
+Console.ReadLine();
